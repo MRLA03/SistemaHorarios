766f19dfbd9b452bb438a1c485656eedcdded8ea
AlejandroTrujilloReyna <alejandro.trujillo.reyna@uabc.edu.mx>
2024-04-01 10:11:01 -0700

    Agregue un poco sobre asignatura

---
+++ SistemaHorarios/persistencia/src/main/java/mx/dreamcatchersoftware/DAO/AsignaturaDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.dreamcatchersoftware.DAO;
+import mx.dreamcatchersoftware.entidad.Asignatura;
+import mx.dreamcatchersoftware.persistencia.AbstractDAO;
+/**
+ *
+ * @author Usuario
+ */
+public class AsignaturaDAO extends AbstractDAO<Integer, Asignatura> {
+    
+}
